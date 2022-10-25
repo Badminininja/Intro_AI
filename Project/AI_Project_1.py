7ea54c406775f00c19932d35862b27a02094837f
Joseph M <josephnm911@gmail.com>
2022-10-25 14:11:14 -0700

    python things .-.

---
+++ Project/AI_Project_1.py
@@ -0,0 +1,34 @@
+from gettext import npgettext
+from queue import PriorityQueue
+from tkinter.tix import CheckList
+import numpy as np
+
+print ('hello world')
+
+#   https://www.codingem.com/numpy-compare-arrays/#:~:text=The%20easiest%20way%20to%20compare,if%20the%20elements%20are%20True. help for comparing arrays
+#   function general-search(problem, QUEUEING-FUNCTION)
+#       nodes = MAKE-QUEUE(MAKE_NODE(problem.INITIAL-STATE))
+#       loop do
+#           if EMPTY(nodes) then return "failure"
+#           node = REMOVE-FRONT(nodes)
+#           if problme.GOAL-TEST(node.STATE) succeeds then return node
+#           nodes = QUEUEING-FUNCTION(nodes, EXPAND(nod,problem.OPERATORS))
+#   end
+#
+#                      GENERAL SEARCH ALGORITHM
+#
+#                   8-puzzle game
+#                   Goals [1,2,3]
+#                         [4,5,6]
+#                         [7,8,0]
+#                   where 0 is considered the placeholder for the empty space
+#                   need to implement, Uniform cost search (A* with h(n)=0), Misplaced tile hueristic, manhattan distance hueristic
+#
+#           Things we need for search algorithms:
+#           Start state, Operators, goal state
+#
+GoalState = npgettext.array([[1,1],[2,2]])
+InitialState = npgettext.array([[1,1],[2,2]])
+
+CheckingArray = (GoalState == InitialState).all()
+print (CheckingArray)
