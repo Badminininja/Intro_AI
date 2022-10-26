138253d3173b61f01ab63ed223e1f4395e8e60d8
Joseph M <josephnm911@gmail.com>
2022-10-25 18:34:05 -0700

    getting the queue to work very simply

--- Project/AI_Project_1.py
+++ Project/AI_Project_1.py
@@ -1,11 +1,11 @@
 from gettext import npgettext
 from queue import PriorityQueue
-from tkinter.tix import CheckList
 import numpy as np
 
 print ('hello world')
 
 #   https://www.codingem.com/numpy-compare-arrays/#:~:text=The%20easiest%20way%20to%20compare,if%20the%20elements%20are%20True. help for comparing arrays
+#   https://docs.python.org/3/library/queue.html The Queues
 #   function general-search(problem, QUEUEING-FUNCTION)
 #       nodes = MAKE-QUEUE(MAKE_NODE(problem.INITIAL-STATE))
 #       loop do
@@ -23,12 +23,47 @@ print ('hello world')
 #                         [7,8,0]
 #                   where 0 is considered the placeholder for the empty space
 #                   need to implement, Uniform cost search (A* with h(n)=0), Misplaced tile hueristic, manhattan distance hueristic
-#
+#                   A* works with f(n) = g(n) + h(n) where g(n) is distance from initial to current and g(n) is estimated distance from current to goal
 #           Things we need for search algorithms:
 #           Start state, Operators, goal state
 #
-GoalState = np.array([[1,1],[2,2]])
-InitialState = np.array([[1,1],[2,2]])
+         
+GoalState = np.array([[1,2,3],[4,5,6],[7,8,0]])
+#lets make an arbitrary initial state for now
+#                 Initial [1,0,3]
+#                         [4,2,5]
+#                         [7,8,6]
+InitialState = np.array([[1,0,3],[4,2,5],[7,8,6]]) #later on, make a function that'll randomize the initial state
 
-CheckingArray = (GoalState == InitialState).all()
-print (CheckingArray)
+#CheckingArray = (GoalState == InitialState).all()
+#print (CheckingArray) these can be used to check if its the goal state
+
+
+class nodes:
+    def __init__(self,array) -> None:
+        self.array = array
+        #self.children = children
+        #self.parents = parents
+        pass
+
+def AStar(StartingState, QueueingFunction):
+    failure = False
+    QueueingFunction = PriorityQueue(0)
+    StartNode = nodes(StartingState)
+    QueueingFunction.put((1,StartNode))
+    while not failure:
+        if QueueingFunction.empty():
+            failure = True
+        else:
+            print('we made it through')
+            failure = True
+
+Q = PriorityQueue(0)
+AStar(InitialState,Q)
+
+    
+
+#       we now have a way to check if something is the goal state
+#       we can use a priority queue as our queuing function since we are expanding the least cost anyway
+
+#
