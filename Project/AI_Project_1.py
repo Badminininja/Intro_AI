c22a592c9b6156fbda4fb9662298d96f74ff921e
Joseph M <josephnm911@gmail.com>
2022-10-25 20:52:38 -0700

    how to check how many children we can have/where

--- Project/AI_Project_1.py
+++ Project/AI_Project_1.py
@@ -35,24 +35,60 @@ GoalState = np.array([[1,2,3],[4,5,6],[7,8,0]])
 #                         [7,8,6]
 InitialState = np.array([[1,0,3],[4,2,5],[7,8,6]]) #later on, make a function that'll randomize the initial state
 
+#{x = np.where(InitialState == 0)       # random work to find out where the 0 is in the state
+#print(x)
+#row = x[0]
+#column = x[1]
+
+#print(InitialState[row[0]][column[0]])
+#actualRow = x[0][0]
+#print(actualRow)
+#actualColoumn = x[1][0]
+#print(actualColoumn)
+#print(InitialState[actualRow][actualColoumn])
 #CheckingArray = (GoalState == InitialState).all()
 #print (CheckingArray) these can be used to check if its the goal state
-
+#       Failed numpy array is differnet from normal array
+#for row in InitialState:
+#    for element in row:
+#        if(element == 0):
+#            BlankRow = row
+#            BlankColoumn = element
+#print(InitialState.index(BlankRow))
+#print(BlankRow.index(0))}
 
 class nodes:
-    def __init__(self,array) -> None:
+    def __init__(self,array):
         self.array = array
         #self.children = children
         #self.parents = parents
-        ChildCount = 0
-        pass
+        self.ChildCount = 0
+        tmp = np.where(array == 0)
+        self.BlankRow = tmp[0][0] #would output as an array with a single element, get the value byt doing BlankRow[0]
+        self.BlankColumn = tmp[1][0]
+        #find location of blank (0)
+
+        
     def getValue(self):
         print(self.array)
 
-        pass
-    def createChildren(self):
         
-        pass
+    def createChildren(self):
+        if (self.BlankRow)-1 >=0: #we can go up
+            print("we can go up")
+            self.ChildCount+=1
+        if (self.BlankRow)+1 <=2: #we can go down
+            print("we can go down")
+            self.ChildCount+=1
+        if (self.BlankColumn)-1 >=0: #we can go Left
+            print("we can go Left")
+            self.ChildCount+=1
+        if (self.BlankColumn)+1 <=2: #we can go Right
+            print("we can go Right")
+            self.ChildCount+=1
+        print("we can have " + str(self.ChildCount) + " children")
+
+        
 
 def AStar(StartingState, QueueingFunction, Goal): #function general-search(problem, QUEUEING-FUNCTION)
     failure = False
@@ -67,10 +103,11 @@ def AStar(StartingState, QueueingFunction, Goal): #function general-search(probl
             return False                            
         print('we made it through')     
         temp = QueueingFunction.get()       #node = REMOVE-FRONT(nodes)    temp[1].getValue() this is how we can print the values
-        CheckingArray = (Goal == temp[1])     
+        CheckingArray = (Goal == temp[1]).all()     
         if(CheckingArray ==True):           #if problem.GOAL-TEST(node.STATE) succeeds then return node
             return temp
-        else:                               #nodes = QUEUEING-FUNCTION(nodes, EXPAND(nod,problem.OPERATORS))
+        else:
+            temp[1].createChildren()           #nodes = QUEUEING-FUNCTION(nodes, EXPAND(nod,problem.OPERATORS))
             
 
 
