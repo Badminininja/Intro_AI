3da83e77711365d16518cbd3591b7c2a20b0aa98
Joseph M <josephnm911@gmail.com>
2022-11-01 15:25:36 -0700

    made the menu for the hard-coded problems

--- Project/AI_Project_1.py
+++ Project/AI_Project_1.py
@@ -22,13 +22,7 @@ nodesExpanded = 0
 GoalState = np.array([[1,2,3],[4,5,6],[7,8,0]])
 
 #later on, make a function that'll randomize the initial state
-#InitialState = np.array([[1,2,3],[4,5,6],[0,7,8]]) #Depth 2
-#InitialState = np.array([[1,2,3],[5,0,6],[4,7,8]]) #Depth 4
-#InitialState = np.array([[1,3,6],[5,0,2],[4,7,8]]) #Depth 8
-#InitialState = np.array([[1,3,6],[5,0,7],[4,8,2]]) #Depth 12
-InitialState = np.array([[1,6,7],[5,0,3],[4,8,2]]) #Depth 16
-#InitialState = np.array([[7,1,2],[4,8,5],[6,3,0]]) #Depth 20
-#InitialState = np.array([[0,7,2],[4,6,1],[3,5,8]]) #Depth 24
+
 
 class nodes:
     def __init__(self,Array):
@@ -249,6 +243,47 @@ def AStarManHatDist(StartingState, QueueingFunction, Goal): #majority same as pr
 startTime = time.time()
 depth = 0
 Q = list(())
+menu = input("Welcome to Joseph's 8-puzzle solver. Type '1' for a hard-coded puzzle, or '2' to create you're own\n") #this looks similar to the example shown on the project example for simplicity and clarity
+if(menu == '1'):
+    InitialState1 = np.array([[1,2,3],[4,5,6],[0,7,8]]) #Depth 2
+    print("Problem 1: depth 2")
+    print(InitialState1)
+    InitialState2 = np.array([[1,2,3],[5,0,6],[4,7,8]]) #Depth 4
+    print("Problem 2: depth 4")
+    print(InitialState2)
+    InitialState3 = np.array([[1,3,6],[5,0,2],[4,7,8]]) #Depth 8
+    print("Problem 3: depth 8")
+    print(InitialState3)
+    InitialState4 = np.array([[1,3,6],[5,0,7],[4,8,2]]) #Depth 12
+    print("Problem 4: depth 12")
+    print(InitialState4)
+    InitialState5 = np.array([[1,6,7],[5,0,3],[4,8,2]]) #Depth 16
+    print("Problem 5: depth 16")
+    print(InitialState5)
+    InitialState6 = np.array([[7,1,2],[4,8,5],[6,3,0]]) #Depth 20
+    print("Problem 6: depth 20")
+    print(InitialState6)
+    InitialState7 = np.array([[0,7,2],[4,6,1],[3,5,8]]) #Depth 24
+    print("Problem 7: depth 24")
+    print(InitialState7)
+    codedP = input("These are EK's hard coded examples, please type the problem number for which one you'd like to run. Not the depth\n")
+    match codedP:
+        case '1':
+            InitialState = InitialState1
+        case '2':
+            InitialState = InitialState2
+        case '3':
+            InitialState = InitialState3
+        case '4':
+            InitialState = InitialState4
+        case '5':
+            InitialState = InitialState5
+        case '6':
+            InitialState = InitialState6
+        case '7':
+            InitialState = InitialState7
+elif(menu == '2'):
+    print("type the puzzle")
 answer = AStarMisplacedTile(InitialState,Q,GoalState)
 depth = answer.Gn
 outputList = list(())
